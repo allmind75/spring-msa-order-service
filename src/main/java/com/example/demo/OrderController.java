ad72cc1b32afb56f2687a184f8a2bd364327e614
장환호 <allmind75@interpark.com>
2020-12-02 13:23:36 +0900

    config 설정 및 logback-spring.xml 추가

---
+++ src/main/java/com/example/demo/OrderController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+public class OrderController {
+
+    @GetMapping("/order")
+    public String order() {
+        log.debug("order service log test");
+        return "order service";
+    }
+}
